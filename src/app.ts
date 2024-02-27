084425cd71e7a2df7b315ad12a728a5181f1dfc4
TH-Raju <rajukhan894200@gmail.com>
2024-02-28 05:10:23 +0600

    [Done] users controller and route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 const app: Application = express()
 import cors from 'cors'
+import userRouter from './app/modules/users/users.route'
 
 app.use(cors())
 
@@ -8,7 +9,11 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: Response) => {
+// Application routes
+
+app.use('/api/v1/users', userRouter)
+
+app.get('/', async (req: Request, res: Response) => {
   res.send('Server working successfully')
 })
 
--- src/app/modules/users/users.utils.ts
+++ src/app/modules/users/users.utils.ts
@@ -11,5 +11,7 @@ export const findLastUserId = async () => {
 
 export const generateUserId = async () => {
   const currentId = (await findLastUserId()) || (0).toString().padStart(5, '0')
-  return currentId
+  // increment by 1
+  const incrementedId = (parseInt(currentId) + 1).toString().padStart(5, '0')
+  return incrementedId
 }
