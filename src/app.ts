736728f78d68a0ea1016b7d1fde93d9fbd249fe6
TH-Raju <rajukhan894200@gmail.com>
2024-02-29 01:40:23 +0600

    [!Done] implementing Error handling

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Application, NextFunction, Request, Response } from 'express'
 const app: Application = express()
 import cors from 'cors'
 import userRouter from './app/modules/users/users.route'
@@ -13,8 +13,37 @@ app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/v1/users', userRouter)
 
-app.get('/', async (req: Request, res: Response) => {
-  res.send('Server working successfully')
+// Error
+class ApiError extends Error {
+  statusCode: number
+
+  constructor(statusCode: number, message: string | undefined, stack = '') {
+    super(message)
+    this.statusCode = statusCode
+    if (stack) {
+      this.stack = stack
+    } else {
+      Error.captureStackTrace(this, this.constructor)
+    }
+  }
+}
+
+app.get('/', async (req: Request, res: Response, next: NextFunction) => {
+  throw new ApiError('Oreee Error re..!')
+  // next('ore Error re..!')
+})
+
+// global error handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    res.status(400).json({
+      error: err,
+    })
+  } else {
+    res.status(500).json({
+      error: 'Something went wrong',
+    })
+  }
 })
 
 export default app
