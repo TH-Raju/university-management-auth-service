de21220ac10ace89b9e3911398d3ff3d2e6b7811
TH-Raju <rajukhan894200@gmail.com>
2024-03-04 03:37:12 +0600

    [Done] handle not found route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
 import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import router from './app/routes';
+import httpStatus from 'http-status';
 
 app.use(cors());
 
@@ -20,5 +21,19 @@ app.get('/', (req, res) => {
 
 // global error handler
 app.use(globalErrorHandler);
+
+// handle not found route
+app.use('*', (req: Request, res: Response) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found',
+    errorMessages: [
+      {
+        path: req.originalUrl,
+        message: 'API Not Found',
+      },
+    ],
+  });
+});
 
 export default app;
